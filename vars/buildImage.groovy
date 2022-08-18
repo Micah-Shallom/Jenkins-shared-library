7dbbc146a9d371051dd9ed231d23a2ae0180fa08
Micah Shallom <micahshallom@gmail.com>
2022-08-18 13:32:23 +0100

    changed repo

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -8,8 +8,8 @@ def call(){
             passwordVariable: "PASSWD"
         )
     ]) {
-        sh "docker build -t mshallom/java-app:jma-2.0 ."
+        sh "docker build -t mshallom/test-repo:jma-2.0 ."
         sh "echo $PASSWD | docker login -u $USER --password-stdin"
-        sh "docker push mshallom/java-app:jma-2.0"
+        sh "docker push mshallom/test-repo:jma-2.0"
     }
 }
